d2a4068529785dcef6273d649cfdf69525169381
Daniel Ramos <danramos429@gmail.com>
2018-06-19 16:22:55 -0400

    update RateIterator and Reader

--- src/RateIterator.js
+++ src/RateIterator.js
@@ -1,48 +1,30 @@
-export default class RateIterator{
-   constructor(array, callback, delay){
-       this.index = 0;
-       this.delay = delay;
-       this.array = array;
-       this.iteratorId = null;
-       this.callback = callback;
-       this.paused = false;
-       this.resume();
-   }
-
-   pause(){
-    clearTimeout(this.iteratorId)
-   }
-
-   resume(){
-        this.iteratorId = setTimeout(() =>{
-        if(this.index == this.array.length){
-          clearTimeout(this.iteratorId)
-        }else{
-            this.resume()
-            this.callback(this.array[this.index])
-            this.index++;
-        }
-      }, this.delay)
-   }
-
-}
-
-var b = ["A", "B", "C", "D", "E", "F", "G", "H"];
-
-var counter = 0;
-var a = new RateIterator(b,(item)=>{ 
-    console.log("ITEM IS ", item);
-    if(counter === 2){
-        a.pause()
+export default class RateIterator {
+    constructor(array, callback, delay) {
+      this.index = 0;
+      this.delay = delay || 200;
+      this.array = array || [];
+      this.iteratorId = null;
+      this.callback = callback;
     }
-    counter++;
-
-}, 1000 )
-console.log("TEST")
-
-setTimeout(() =>{
-    a.resume()
-}, 5000)
-
-//a.resume()
-
+  
+    pause() {
+      clearTimeout(this.iteratorId);
+    }
+  
+    clear() {
+      clearTimeout(this.iteratorId);
+      this.index = 0;
+    }
+  
+    start() {
+      this.iteratorId = setTimeout(() => {
+        if (this.index === this.array.length) {
+          clearTimeout(this.iteratorId);
+        } else {
+          this.callback(this.array[this.index], this.index, this.array);
+          this.start();
+          this.index++;
+        }
+      }, this.delay);
+    }
+  }
--- src/Reader.js
+++ src/Reader.js
@@ -1,120 +1,92 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import RateIterator from './RateIterator'
+import React from "react";
+import PropTypes from "prop-types";
+import RateIterator from "./RateIterator";
 
-const PERIOD = "."
-const PERIOD_PAUSE_IN_MS=1000
+const PERIOD = ".";
+const PERIOD_PAUSE_IN_MS = 1000;
 export default class Reader extends React.PureComponent {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      word : "",
-      wpm : "",
-      text : "",
-      words : "",
-      paused: false,
-    }
+      word: "",
+      wpm: "",
+      text: "",
+      words: "",
+    };
+    console.log("START");
   }
-  componentWillReceiveProps(props){
-    this.setState({ text: props.text})
+  componentWillReceiveProps(props) {
+    this.setState({ text: props.text });
   }
-  formatText = (text) =>{
-    var words = []
-    text.split(" ").forEach((t)=>{ 
-      if(t.endsWith('.')){
-        words.push(t.substring(0, t.length-1))
-        words.push(t.charAt(t.length-1))
-      }else{
+  formatText = text => {
+    var words = [];
+    text.split(" ").forEach(t => {
+      if (t.endsWith(".")) {
+        words.push(t.substring(0, t.length - 1));
+        words.push(t.charAt(t.length - 1));
+      } else {
         words.push(t);
       }
-    })
+    });
     return words;
-  }
+  };
 
-  recurringTimer = (i=0) =>{
-    const { words } = this.state;
-    let interval = setInterval(() =>{
-      if(i == words.length){
-        clearInterval(interval)
-        return;
-      }
-      if(words[i] === PERIOD){
-        this.setState({word : PERIOD}, ()=>{
-          clearInterval(interval)
-          i++;
-          setTimeout(()=>{
-            this.recurringTimer(i)
-          }, PERIOD_PAUSE_IN_MS)
-        })
-      }else{
-        this.setState({word : words[i]}, ()=>{
-          i++;
-        })
-      }
-    }, this.wpmToIntervalMS(this.state.wpm))
-  }
+  wpmToIntervalMS = wpm => {
+    return Math.floor(1 / (parseInt(wpm) / 60) * 1000);
+  };
 
-  renderWords = () => {
-    if(this.state.wpm === "") return;
-    this.setState({ words : this.formatText(this.props.text)}, () =>{
-      //this.recurringTimer()
-      this.iterator = new RateIterator(this.state.words, (word) =>{
-        if(!this.state.paused){
-          this.setState({word})
-        }
-        
-      }, this.wpmToIntervalMS(this.state.wpm))
-    })
+  run = () => {
+    if (this.state.wpm === "") return;
+    if (!this.iterator) {
+      this.iterator = new RateIterator(
+        this.formatText(this.props.text),
+        word => { this.setState({ word })},
+        this.wpmToIntervalMS(this.state.wpm)
+      );
+    }
+    this.iterator.start();
+    
+  };
+  pause = () => {
+    this.iterator.pause();
+  };
+  onWpmChange = e => {
+    this.setState({ wpm: e.target.value });
+  };
 
-  }
-
-  wpmToIntervalMS = (wpm) =>{
-    return Math.floor(1 / ( parseInt(wpm) / 60 ) * 1000)
-  }
-
-  run = () =>{
-    this.renderWords();
-  }
-  pause = () =>{
-   this.setState({paused : true}, () =>{
-      this.iterator.pause()
-   })
-  }
-  onWpmChange = (e) =>{
-    this.setState({wpm : e.target.value})
-  }
-  
-
-  render(){
-    return (   
-    <div>
-      <h1>{this.state.word}</h1>
+  render() {
+    return (
       <div>
-        <input 
-          type="number"
-          min="0"
-          step="50"
-          placeholder="Type WPM value, e.g 350" 
-          style={{ width: '200px', height: "30px"}} 
-          value={this.state.wpm}
-          onChange={this.onWpmChange}>
-        </input>
+        <h1>{this.state.word}</h1>
+        <div>
+          <input
+            type="number"
+            min="0"
+            step="50"
+            placeholder="Type WPM value, e.g 350"
+            style={{ width: "200px", height: "30px" }}
+            value={this.state.wpm}
+            onChange={this.onWpmChange}
+          />
+        </div>
+        <br />
+        <span>
+          <button style={{ width: "100px", height: "30px" }} onClick={this.run}>
+            Run
+          </button>
+
+          <button
+            style={{ width: "100px", height: "30px" }}
+            onClick={this.pause}
+          >
+            Pause
+          </button>
+        </span>
       </div>
-      <br/>
-      <span>
-        <button style={{ width: '100px', height: "30px"}}
-        onClick={this.run}>Run</button>
-
-        <button style={{ width: '100px', height: "30px"}}
-        onClick={this.pause}>Pause</button>
-
-      </span>
-      
-    </div>
-    )
+    );
   }
 }
 
 Reader.propTypes = {
-  text : PropTypes.string.isRequired
-}
+  text: PropTypes.string.isRequired
+};
